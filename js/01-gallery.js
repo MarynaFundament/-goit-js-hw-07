e2233df089ef2e2a6853ddd4ed4364dbd2d77f40
MarynaFundament <marina.fundament@ukr.net>
2023-03-05 17:28:47 +0200

    7 hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const divInsert = document.querySelector(`.gallery`)
+// console.log(divInsert)
+const cardsMarkup = createDivEl(galleryItems);
+divInsert.insertAdjacentHTML(`beforeend`, cardsMarkup )
+
+function createDivEl(items){
+    const markup = galleryItems.map(({preview,original,description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    });
+    return markup.join('');
+  }
+  
+//   console.log(createDivEl(galleryItems));
+
+  divInsert.addEventListener(`click`, handleClickElement)
+
+  
+  function handleClickElement(event) {
+    event.preventDefault();
+  
+    const targetEl = event.target;
+    const targetPicture = targetEl.classList.contains('gallery__image');
+    if (!targetPicture) {
+      return;
+    }
+  
+    const imageSrc = targetEl.dataset.source;
+    const lightbox = basicLightbox.create(`
+      <img src="${imageSrc}" />
+    `);
+  
+    lightbox.show();
+  }
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divInsert = document.querySelector(`.gallery`)
+// console.log(divInsert)
+const cardsMarkup = createDivEl(galleryItems);
+divInsert.insertAdjacentHTML(`beforeend`, cardsMarkup )
+
+function createDivEl(items){
+    const markup = galleryItems.map(({preview,original,description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" 
+       src="${preview}"
+       alt="${description}" />
+    </a>`;
+    });
+    return markup.join('');
+  }
+
+
+  divInsert.addEventListener(`click`, handleClickElement)
+
+  
+  function handleClickElement(event) {
+    event.preventDefault();
+  
+    const targetEl = event.target;
+    const targetPicture = targetEl.classList.contains('gallery__image');
+    if (!targetPicture) {
+      return;
+    }
+  
+    
+  }
+  
